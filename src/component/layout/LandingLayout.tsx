76b324c504438a4d7e56928a8d1844d95714e11b
xWyvernPx <phonglethanh2@gmail.com>
2023-05-14 14:35:56 +0700

    [Setup] Error tracking setup

--- src/component/layout/LandingLayout.tsx
+++ src/component/layout/LandingLayout.tsx
@@ -2,10 +2,16 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 
 const LandingLayout = () => {
+  console.log(import.meta.env.SENTRY_DSN);
+
   return (
     <div>
       <header className="w-full h-20 bg-blue-600">Header</header>
       <main>
+        <button onClick={() => methodDoesNotExist()}>
+          Break the world {import.meta.env.VITE_SENTRY_DSN}
+        </button>
+
         <Outlet></Outlet>
       </main>
     </div>
