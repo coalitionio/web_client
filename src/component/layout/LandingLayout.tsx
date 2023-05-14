01a86a8aab1101d1d225872ca9aba1388dd135db
xWyvernPx <phonglethanh2@gmail.com>
2023-05-14 19:55:05 +0700

    [Setup] i18n + graphql + react-query"

--- src/component/layout/LandingLayout.tsx
+++ src/component/layout/LandingLayout.tsx
@@ -1,15 +1,16 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 import { Outlet } from "react-router-dom";
 
 const LandingLayout = () => {
   console.log(import.meta.env.SENTRY_DSN);
-
+  const { t } = useTranslation();
   return (
     <div>
       <header className="w-full h-20 bg-blue-600">Header</header>
       <main>
         <button onClick={() => methodDoesNotExist()}>
-          Break the world {import.meta.env.VITE_SENTRY_DSN}
+          Break the world {t("test")} {import.meta.env.VITE_SENTRY_DSN}
         </button>
 
         <Outlet></Outlet>
--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./route/Route.tsx";
-import * as Sentry from "@sentry/react";
-
-Sentry.init({
-  dsn: import.meta.env.VITE_SENTRY_DSN,
-  integrations: [
-    new Sentry.BrowserTracing(),
-    new Sentry.Replay(),
-    new Sentry.BrowserProfilingIntegration(),
-  ],
-  // Performance Monitoring
-  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
-  // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-});
+import "./utils/lang/i18n.ts";
+import "./utils/errorTracking/sentry";
+import {
+  ReactQueryDevtools,
+  ReactQueryDevtoolsPanel,
+} from "react-query/devtools";
+import { QueryClient, QueryClientProvider } from "react-query";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    {/* <RouterProvider router={router} > */}
-    <App />
-    {/* </RouterProvider> */}
+    <QueryClientProvider client={new QueryClient()}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
